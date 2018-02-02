8739b7b4e120c808de56aa4b6038f5f794f15731
thinkholic <mailtokasun@gmail.com>
2018-02-02 11:01:40 +0530

    Add prettier

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,8 @@
-
 module.exports = (req, res, next) => {
-  const token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
+  const token =
+    (req.body && req.body.access_token) ||
+    (req.query && req.query.access_token) ||
+    req.headers["x-access-token"];
 
   // TODO: Check for valid access token
   // If valid, continue with `next()`
@@ -9,13 +11,12 @@ module.exports = (req, res, next) => {
   // TODO: Implements JWT auth later
 
   if (!token) {
-    res.status(403)
-      .send({
-        status: {
-          code: 403,
-          message: 'Not Authorized'
-        }
-      });
+    res.status(403).send({
+      status: {
+        code: 403,
+        message: "Not Authorized"
+      }
+    });
   }
 
   if (token) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,52 +1,48 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 // NOTE: This is some sample routes only.
 // Should add routes and actions as needed.
 
-router.get('/v1/ping', (req, res) => {
-  res.status(200)
-    .send({
-      status: {
-        code: 200,
-        message: 'OK'
-      },
-      data: req.query
-    });
+router.get("/v1/ping", (req, res) => {
+  res.status(200).send({
+    status: {
+      code: 200,
+      message: "OK"
+    },
+    data: req.query
+  });
 });
 
-router.post('/v1/ping', (req, res) => {
-  res.status(200)
-    .send({
-      status: {
-        code: 200,
-        message: 'OK'
-      },
-      data: req.body
-    });
+router.post("/v1/ping", (req, res) => {
+  res.status(200).send({
+    status: {
+      code: 200,
+      message: "OK"
+    },
+    data: req.body
+  });
 });
 
-router.put('/v1/ping', (req, res) => {
-  res.status(200)
-    .send({
-      status: {
-        code: 200,
-        message: 'OK'
-      },
-      data: req.body
-    });
+router.put("/v1/ping", (req, res) => {
+  res.status(200).send({
+    status: {
+      code: 200,
+      message: "OK"
+    },
+    data: req.body
+  });
 });
 
-router.delete('/v1/ping', (req, res) => {
-  res.status(200)
-    .send({
-      status: {
-        code: 200,
-        message: 'OK'
-      },
-      data: req.body
-    });
+router.delete("/v1/ping", (req, res) => {
+  res.status(200).send({
+    status: {
+      code: 200,
+      message: "OK"
+    },
+    data: req.body
+  });
 });
 
 module.exports = router;
